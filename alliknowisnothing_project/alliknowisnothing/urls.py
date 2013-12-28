51b050665c9f095a87988acc48a37f014517e26b
Tam Le <tamsgit@gmail.com>
2013-12-28 17:06:16 -0500

    Set up blog and layout.html

--- alliknowisnothing_project/alliknowisnothing/urls.py
+++ alliknowisnothing_project/alliknowisnothing/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.http.response import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
+
+    # make ze home page redirect to ze blog
+    url(r'^$', 'blog.views.redirect_to_blog_home'),
+
 )
--- alliknowisnothing_project/blog/views.py
+++ alliknowisnothing_project/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def home(request):
+
+    template = loader.get_template('blog/index.html')
+    context = RequestContext(request, {
+                                       }
+    )
+
+    return HttpResponse(template.render(context))
+
+
+def redirect_to_blog_home(request):
+    return HttpResponseRedirect(reverse('blog.views.home'))
+
+
+def archive(request):
+
+    return HttpResponse('The archive!')
